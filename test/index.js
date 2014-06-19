59d48d6480e502a68d245ef46354444c62758559
Ben Drucker <bvdrucker@gmail.com>
2014-06-19 06:56:05 -0400

    rearrange tests

---
+++ test/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var angular = require('angular');
+
+describe('bd.form-state', function () {
+
+  beforeEach(angular.mock.module(require('../')));
+
+  describe('bdSubmit', require('./bd-submit'));
+
+});
