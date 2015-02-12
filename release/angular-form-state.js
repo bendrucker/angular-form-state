9880841f674777a7b5c46b5625b53f76fe0889b9
Ben Drucker <bvdrucker@gmail.com>
2015-02-12 13:47:56 -0500

    v2.0.0 UMD bundle

---
+++ release/angular-form-state.js
@@ -0,0 +1,116 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.angularFormState=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
+'use strict';
+
+module.exports = (typeof window !== "undefined" ? window.angular : typeof global !== "undefined" ? global.angular : null)
+  .module('bd.form-state', [])
+  .directive('bdSubmit', require('./bd-submit'))
+  .directive('submitButton', require('./submit-button'))
+  .name;
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"./bd-submit":2,"./submit-button":3}],2:[function(require,module,exports){
+'use strict';
+
+module.exports = function ($parse, $q, $exceptionHandler) {
+
+  function SubmissionController () {
+    this.succeeded = false;
+    this.failed = false;
+    this.pending = false;
+    this.attempted = false;
+    this.attempts = 0;
+    this.error = null;
+  }
+
+  SubmissionController.prototype.setPending = function () {
+    this.pending = true;
+    this.succeeded = false;
+    this.failed = false;
+    this.attempted = true;
+    this.attempts++;
+    this.error = null;
+  };
+
+  SubmissionController.prototype.setSuccess = function () {
+    this.succeeded = true;
+    this.failed = false;
+    this.pending = false;
+    this.error = null;
+  };
+
+  SubmissionController.prototype.setFailure = function (err) {
+    this.succeeded = false;
+    this.failed = true;
+    this.pending = false;
+    this.error = err;
+    $exceptionHandler(err);
+  };
+
+  return {
+    require: ['form', 'bdSubmit'],
+    controller: SubmissionController,
+    compile: function () {
+      return {
+        pre: function (scope, element, attributes, controllers) {
+          var formController = controllers[0];
+          var submissionController = controllers[1];
+
+          formController.submission = submissionController;
+        },
+        post: function (scope, element, attributes, controllers) {
+          var submissionController = controllers[1];
+          var fn = $parse(attributes.bdSubmit);
+          element.on('submit', function (event) {
+            var result;
+            scope.$apply(function () {
+              result = fn(scope, {$event: event});
+              submissionController.setPending();
+            });
+            $q.when(result)
+              .then(function () {
+                submissionController.setSuccess();
+              })
+              .catch(function (err) {
+                submissionController.setFailure(err);
+              });
+          });
+        }
+      };
+    }
+  };
+
+};
+
+module.exports.$inject = ['$parse', '$q', '$exceptionHandler'];
+
+},{}],3:[function(require,module,exports){
+'use strict';
+
+module.exports = function ($interpolate, $parse) {
+  return {
+    require: '^bdSubmit',
+    restrict: 'A',
+    compile: function (element, attributes) {
+      if (!attributes.type) {
+        attributes.$set('type', 'submit');
+      }
+      return function (scope, element, attributes, controller) {
+        var original = element.text();
+        function ngDisabled () {
+          return attributes.ngDisabled && !!$parse(attributes.ngDisabled)(scope);
+        }
+        scope.$watch(function () {
+          return controller.pending;
+        }, function (pending) {
+          attributes.$set('disabled', pending || ngDisabled());
+          element.text($interpolate(pending ? attributes.pending : original)(scope));
+        });
+      };
+    }
+  };
+};
+module.exports.$inject = ['$interpolate', '$parse'];
+
+},{}]},{},[1])(1)
+});
